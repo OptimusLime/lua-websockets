6db0080d6dfa3a6f528b4947d007adf208316440
Paul Szerlip <paul@geometric.ai>
2015-09-17 15:16:39 -0400

    Timeout different for listener and oscket .

--- src/websocket/server_ev.lua
+++ src/websocket/server_ev.lua
@@ -166,7 +166,7 @@ local listen = function(opts)
   if not listener then
     error(err)
   end
-  listener:settimeout(opts.timeout or 1)
+  listener:settimeout(opts.timeoutListener or 0)
   
   self.sock = function()
     return listener
@@ -175,7 +175,7 @@ local listen = function(opts)
   local listen_io = ev.IO.new(
     function()
       local client_sock = listener:accept()
-      client_sock:settimeout(opts.timeout or 1)
+      client_sock:settimeout(opts.timeoutClient or 0)
       assert(client_sock)
       local request = {}
       local last
